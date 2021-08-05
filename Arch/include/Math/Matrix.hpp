76e8f954041b0330ac9260e9950e43e91c786d06
yorlend <yorlend@github.com>
2021-08-05 17:11:41 +0300

    Arch matrix done

--- Arch/include/Math/Matrix.hpp
+++ Arch/include/Math/Matrix.hpp
@@ -1,15 +1,31 @@
 #ifndef MATRIX_HPP
 #define MATRIX_HPP
 
+#include <Core/common/math/Mat.hpp>
 #include "Math/Vector.hpp"
 
 class Matrix
 {
 public:
     Matrix();
+    Matrix(const Core::Mat& mat);
 
-    static Matrix make_mat_id();
-    static Matrix make_mat_translation(double dx, double dy, double dz);
+    static Matrix identity();
+    static Matrix translate(double dx, double dy, double dz);
+    static Matrix translate(const Vector& offset);
+    static Matrix rotate(const Vector& axis, double angle);
+    static Matrix scale(double fx, double fy, double fz);
+    static Matrix scale(const Vector& factors);
+    static Matrix perspective(double fov, double near, double far);
+
+    double det() const;
+    Matrix operator*(const Matrix& right) const;
+    Matrix operator*(double val) const;
+    Vector operator*(const Vector& vec) const;
+    operator Core::Mat() const;
+
+    Matrix transpose() const;
+    Matrix inverse() const;
 
 protected:
     explicit Matrix(const double mat[16]);
@@ -17,5 +33,8 @@ protected:
 private:
     double data[16];
 };
+
+Matrix operator*(double val, const Matrix& mat);
+Vector operator*(const Vector& vec, const Matrix& mat);
 
 #endif //MATRIX_HPP
